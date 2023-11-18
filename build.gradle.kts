fc0709f350e82f45287a89972aa42c394d0cecfc
Scott Parlane <scott@parlanenz.com>
2023-11-18 16:43:36 +1300

    Add dependency on retrofit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    extra.apply {
+        set("retrofit_version", "2.9.0")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.1.3" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
