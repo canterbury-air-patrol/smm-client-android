8da080b7cb973930b14edd921a35ce9241399f77
Scott Parlane <scott@parlanenz.com>
2023-12-14 20:51:04 +1300

    Upgrade to grade 8.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.1.3" apply false
+    id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
 
