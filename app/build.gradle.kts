1c95676780ebb02d6c93886133fecaf9d1e69a84
Scott Parlane <scott@parlanenz.com>
2024-01-06 17:08:38 +1300

    Update activity-compose to 1.8.2
    Also requires the sdk compiler to be updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "org.canterburyairpatrol.smmclient"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "org.canterburyairpatrol.smmclient"
@@ -53,7 +53,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
