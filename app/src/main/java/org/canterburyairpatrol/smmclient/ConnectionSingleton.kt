c528d533dc284af2f09ea7f1ed4f93d45bdbd537
Scott Parlane <scott@parlanenz.com>
2024-01-28 23:23:06 +1300

    Create singleton for SMMAPI connection
    This also checks if an error occurs and refuses to progress from the
    login screen, to warn the user early if there is an issue.

---
+++ app/src/main/java/org/canterburyairpatrol/smmclient/ConnectionSingleton.kt
@@ -0,0 +1,38 @@
+package org.canterburyairpatrol.smmclient
+
+import org.canterburyairpatrol.smmclient.data.SMMConnectionDetails
+import org.canterburyairpatrol.smmclient.smm.SMMAPI
+import org.canterburyairpatrol.smmclient.smm.SMMConnectionInstance
+
+class ConnectionSingleton private constructor() {
+    private lateinit var connectionDetails : SMMConnectionDetails
+    private lateinit var API : SMMAPI
+
+    fun setConnectionDetails(details: SMMConnectionDetails)
+    {
+        this.connectionDetails = details
+    }
+
+    suspend fun connect() : String
+    {
+        try {
+            this.API = (SMMConnectionInstance(this.connectionDetails).getAPI())
+            return ""
+        } catch (e: Exception) {
+            return e.message ?: "Unknown error"
+        }
+    }
+    fun getAPI() : SMMAPI {
+        return this.API
+    }
+    companion object {
+        @Volatile
+        private var instance: ConnectionSingleton? = null
+
+        fun getInstance(): ConnectionSingleton {
+            return instance ?: synchronized(this) {
+                instance ?: ConnectionSingleton().also { instance = it }
+            }
+        }
+    }
+}
