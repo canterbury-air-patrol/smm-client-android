c4bfb194f223572cf9e68405d92f34baf800fc98
Scott Parlane <sjp@canterburyairpatrol.org>
2024-04-25 22:17:24 +1200

    Update targetSdk to 34 (android 14) (#37)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "org.canterburyairpatrol.smmclient"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
