e6cee41dfa119d60e86d6e9d78185227290669d0
Scott Parlane <scott@parlanenz.com>
2024-05-28 23:08:31 +1200

    Upgrade to kotlin 1.9.24
    This requires upgrading to the kotlin extension version 1.5.14

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
 
 plugins {
     id("com.android.application") version "8.4.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
 
 tasks.register("clean", Delete::class) {
