05ee5a96fcd71776593b2fd0ff579a1a43f3a8b0
Scott Parlane <sjp@canterburyairpatrol.org>
2024-09-01 20:07:39 +1200

    Switch to Kotlin 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,8 @@ buildscript {
 
 plugins {
     id("com.android.application") version "8.6.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
 }
 
 tasks.register("clean", Delete::class) {
