965983eafc3eed9bf3e661e316a7aced44c2e5a5
Scott Parlane <sjp@canterburyairpatrol.org>
2024-09-01 22:51:12 +1200

    Switch to Version Catalog for plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.6.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
+    alias (libs.plugins.android.application) apply false
+    alias (libs.plugins.kotlin.android) apply false
+    alias (libs.plugins.kotlin.compose) apply false
 }
 
 tasks.register("clean", Delete::class) {
