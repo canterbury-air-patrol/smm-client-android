403ccefc66632ef62235251a97d69a01c0734b72
Scott Parlane <sjp@canterburyairpatrol.org>
2024-09-14 22:18:36 +1200

    Switch to using a version catalog (#51)
    * Switch to using a version catalog for dependencies
    
    * Switch to Version Catalog for plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    extra.apply {
-        set("retrofit_version", "2.11.0")
-        set("okhttp_version", "4.12.0")
-    }
 }
 
 plugins {
-    id("com.android.application") version "8.6.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
+    alias (libs.plugins.android.application) apply false
+    alias (libs.plugins.kotlin.android) apply false
+    alias (libs.plugins.kotlin.compose) apply false
 }
 
 tasks.register("clean", Delete::class) {
