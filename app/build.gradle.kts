34815abb4420003a656e77f24bd2aa3c7846b4db
Scott Parlane <sjp@canterburyairpatrol.org>
2025-07-10 09:06:06 +1200

    Update to SDK 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "org.canterburyairpatrol.smmclient"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "org.canterburyairpatrol.smmclient"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
